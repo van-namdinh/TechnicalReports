a06c680d63cad36e8bfd4b65d80f437e29e89118
yoloh3 <heroh396@gmail.com>
2018-01-21 18:45:40 +0700

    updating on today!

---
+++ updategit.sh
@@ -0,0 +1,14 @@
+# -----------------------------------------------------------------------------
+# Project name   :
+# File name      : updategit.sh
+# Created date   : Th01 21 2018
+# Author         : Van-Nam DINH
+# Last modified  : Th01 21 2018 18:44
+# Guide          :
+# -----------------------------------------------------------------------------
+#!/bin/bash
+
+
+git add .
+git commit -m "updating on today!"
+git push origin
